d16eb70d5874d4dc4b1ddd28b7bfd0d9594a3ba9
EstesJorie <joe.tresise@aol.com>
2025-04-08 11:29:21 +0100

    080425 addition of beginning C files

---
+++ DataTypes/Sources/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int age = 40;
+    double average = 3.141;
+    float specificAge = 40.0f;
+    char grade = 'A';
+    char phrase[] = "The quick brown fox jumps over the lazy dog"; //Array of characters (STRING eqiv)
+
+    return 0;
+}
---
+++ Variables/Sources/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char characterName[] = "Tom";
+    int age = 28;
+    printf("Hello, my name is %s and I am %d years old.\n", characterName, age); //%s insert string of characters, %d insert integer
+    return 0;
+}
